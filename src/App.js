6b255f04abe8c015d8845e5e7a55051329bb79c8
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2021-10-06 08:51:52 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+function getTime(){return (new Date()).toLocaleTimeString()}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       React Day 1
-      </header>
+      <div>
+     <label htmlFor="name">Enter Name:</label>
+     <input id='name'></input>
+     <button style={{backgroundColor:'blue'}}>Click Me</button>
+     <div>{getTime()}</div>
+     </div>
+    <div className="ui container comments">
+      <div className="comment"><a href="/" className="avatar"><img alt="avatar" src="/"/></a>
+     <div className="content"><a href="/" className="author">Author</a>
+     <div className="metadata"><span className="date">{getTime()}</span></div>
+     <div className="text">Nice Blog Post</div></div>
+     
+
+      </div>
     </div>
+    
+    </div>
+    
   );
 }
 
