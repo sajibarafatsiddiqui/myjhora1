51a729e8d9c21f1dab57c94d5032027bb938217c
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2021-10-08 22:49:10 +0600

    Friday 20211008

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Faker from 'faker';
+import Comments from './Comment';
 
-function getTime(){return (new Date()).toLocaleTimeString()}
+
 
 function App() {
   return (
     <div className="App">
-      <div>
-     <label htmlFor="name">Enter Name:</label>
-     <input id='name'></input>
-     <button style={{backgroundColor:'blue'}}>Click Me</button>
-     <div>{getTime()}</div>
-     </div>
-    <div className="ui container comments">
-      <div className="comment"><a href="/" className="avatar"><img alt="avatar" src="/"/></a>
-     <div className="content"><a href="/" className="author">Author</a>
-     <div className="metadata"><span className="date">{getTime()}</span></div>
-     <div className="text">Nice Blog Post</div></div>
-     
-
-      </div>
-    </div>
+    <Comments/>
     
     </div>
     
